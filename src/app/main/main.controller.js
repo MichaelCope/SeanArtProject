fc2bf46f3d7f9f30acb48e96d832df50d5708d99
Michael Cope <mikejcope@gmail.com>
2015-12-23 13:27:40 -0500

    side nav

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,10 +1,9 @@
 /// <reference path="../tsd.d.ts" />
 var MainController = (function () {
     /* @ngInject */
-    function MainController($timeout, toastr) {
+    function MainController($timeout) {
         this.classAnimation = '';
         this.creationDate = 1450810957689;
-        this.toastr = toastr;
     }
     return MainController;
 })();
--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -1,15 +1,17 @@
 /// <reference path="../tsd.d.ts" />
 
+
+
+
 export class MainController {
   public classAnimation: string;
   public creationDate: number;
-  public toastr: any;
+
 
   /* @ngInject */
-  constructor ($timeout: ng.ITimeoutService, toastr: any) {
+  constructor ($timeout: ng.ITimeoutService) {
     this.classAnimation = '';
     this.creationDate = 1450810957689;
-    this.toastr = toastr;
   }
 
 
